7db9d197ccaa46153752a0ca3b522f3a4e8dbf68
JuliusBwan <bwanikajulius2@gmail.com>
2025-04-12 08:50:08 +0100

    my first commit

---
+++ build-a-book-organizer.js
@@ -0,0 +1,7 @@
+const books = [
+  {title: "1984", authorName: "George Orwell", releaseYear: 1935},
+  {title: "The Almanack Of Naval Ravikant", authorName: "Eric Jorgensen", releaseYear: 2021},
+  {title: "Rich Dad Poor Dad", authorName: "Hiyo Kawasaki", releaseYear: 2001},
+  {title: "To Kill a Mockingbird", authorName: "Harper Lee", releaseYear: 1961},
+  {title: "Pride and Prejudice", authorName: "Jane Austen and Anna Quindlen", releaseYear: 1813},
+];
