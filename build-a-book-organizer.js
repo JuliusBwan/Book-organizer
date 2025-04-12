5a7d701afe1280eb23081f5dd1c28a19a488deec
JuliusBwan <bwanikajulius2@gmail.com>
2025-04-12 10:02:21 +0100

    pp

--- build-a-book-organizer.js
+++ build-a-book-organizer.js
@@ -5,3 +5,22 @@ const books = [
   {title: "To Kill a Mockingbird", authorName: "Harper Lee", releaseYear: 1961},
   {title: "Pride and Prejudice", authorName: "Jane Austen and Anna Quindlen", releaseYear: 1813},
 ];
+function sortByYear(book1, book2){
+  if (book1.releaseYear < book2.releaseYear){
+    return -1;
+  }
+  else if (book1.releaseYear === book2.releaseYear){
+    return 0;
+  }
+  return 1;
+}
+
+let filteredBooks = [];
+function filterBooks(arr, year){
+  filteredBooks = arr.filter((book) => book.releaseYear > year);
+  return filteredBooks;
+} 
+console.log(filterBooks(books, 1860));
+
+let sortedBooks = filteredBooks.sort(sortByYear);
+console.log(sortedBooks);
